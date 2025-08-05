582a294a2d4226ba91ca768bbca71f968ff4b296
Pau Sian Sang <paungshinsan27@gmail.com>
2025-08-06 00:07:55 +0630

    feat: unicafe exercise step 4 to 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,45 @@
 import { useState } from 'react'
 
-const Statistics = ({good, neutral, bad}) => {
+const StatisticLine = ({ text, value }) => {
+	return (
+		<tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
+	)
+}
+
+const Statistics = ({ good, neutral, bad }) => {
 	const total = good + neutral + bad
 	const averageScore = ((good * 1) + (neutral * 0) + (bad * -1)) / total
 	const positivePercent = (good / total) * 100
+	if (total > 0) {
+		return (
+			<>
+				<h2>Statistics</h2>
+				<table>
+					<tbody>
+						<StatisticLine text='Good' value={good} />
+						<StatisticLine text='Neutral' value={neutral} />
+						<StatisticLine text='Bad' value={bad} />
+						<StatisticLine text='All' value={total} />
+						<StatisticLine text='Average' value={averageScore} />
+						<StatisticLine text='Positive' value={positivePercent} />
+					</tbody>
+				</table>
+
+			</>
+		)
+	}
 	return (
 		<>
 			<h2>Statistics</h2>
-			<p>Good {good}</p>
-			<p>Neutral {neutral}</p>
-			<p>Bad {bad}</p>
-			<p>All {total}</p>
-			<p>Average {averageScore}</p>
-			<p>Positive {positivePercent}</p>
+			<p>No feedbck given</p>
 		</>
 	)
 }
+
+const Button = ({ onClick, text }) => <button onClick={onClick} >{text}</button>
 
 const App = () => {
 	// save clicks of each button to its own state
@@ -26,9 +50,9 @@ const App = () => {
 	return (
 		<div>
 			<h2>Give Feebacks</h2>
-			<button onClick={() => setGood(good + 1)}>Good</button>
-			<button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-			<button onClick={() => setBad(bad + 1)}>Bad</button>
+			<Button onClick={() => setGood(good + 1)} text='Good' />
+			<Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
+			<Button onClick={() => setBad(bad + 1)} text='Bad' />
 			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	)
